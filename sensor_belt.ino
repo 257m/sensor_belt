97e7e62cd6a1426d36062ada76eb33e8df6e5664
NaitikSilver <naitik.jain07@gmail.com>
2024-10-26 14:59:59 -0400

    added javascript

--- sensor_belt.ino
+++ sensor_belt.ino
@@ -110,6 +110,23 @@ void setup(void) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>First Website</title>
+    <script>
+      $(document).ready(function() {
+            let speed = 0;
+            let maxSpeed = 200;
+
+            setInterval(function() {
+                speed += Math.floor(Math.random() * 20) + 1;
+                if (speed > maxSpeed) {
+                    speed = maxSpeed;
+                }
+
+                let rotation = (speed / maxSpeed) * 180;
+                $('.needle').css('transform', 'translate(-50%, 0) rotate(' + rotation + 'deg)');
+                $('.speed').text(speed);
+            }, 1000);
+        });
+    </script>
     <style>
       #speedometer {
             width: 200px;
